14690871ae0d01a7d851e2e72a3e7b19d7ddb209
fecrakib <rakibislam@132575gmial.com>
2023-01-27 19:21:05 +0600

    data fetch complite

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const productDiv=document.getElementById("all-product");
+// const productPrice=document.getElementById("product-price");
+
+
+
+
+
+fetch("product.json")
+.then(function(res){
+    return res.json();
+})
+.then(function(data){
+  
+    for(let i=0;i<data.length;i++){
+      productDiv.innerHTML +=`
+      <div class="col-lg-4">
+      <div class="single-product">
+          <img src="${data[i].image}" alt="">
+          <h5>$ <span id="product-price">${data[i].price}</span></h5>
+          <h3>${data[i].name}</h3>
+          <p>${data[i].text}.</p>
+          <button>Add To Card</button>
+      </div>
+      `
+    }
+})
+// fetch("product.json")
+// .then(res=>res.json())
+// .then(data=>console.log(data))
